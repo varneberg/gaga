7892ef9335aa7fd5135ce06f1d6ab4b282ff5470
Vegard Arneberg <43135481+varneberg@users.noreply.github.com>
2022-05-05 17:41:36 +0200

    golang gh auth (#4)
    * go-github token test
    
    * golang pipe token
    
    * token pipe
    
    * go workflow
    
    * list all environmental variables
    
    * updated workflow
    
    * go-github auth test
    
    * github token in env
    
    * listenv for debug
    
    * env variables
    
    * prettier print
    
    * testing env difference with different perms
    
    * reapplying permissions
    
    * permissions testing
    
    * custom go step
    
    * new attempt
    
    * auth
    
    * another commit
    
    * gh api url
    
    * manual connection to gh rest api
    
    * removed bearer
    
    * url update
    
    * bad credentials
    
    * accept header
    
    * trying to add label
    
    * json array parsing
    
    * label array
    
    * format json array

--- main.go
+++ main.go
@@ -1,23 +1,97 @@
 package main
 
 import (
-	"context"
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
-	"github.com/google/go-github/github"
-	"golang.org/x/oauth2"
+	"strings"
+	"time"
 )
 
+var ghRepoOwner = os.Getenv("GITHUB_REPOSITORY_OWNER")
+var ghRef = os.Getenv("GITHUB_REF")
+var ghRefName = os.Getenv("GITHUB_REF_NAME")
+var ghRepo = os.Getenv("GITHUB_REPOSITORY")
+var ghToken = os.Getenv("GITHUB_TOKEN")
+var ghEvent = os.Getenv("GITHUB_EVENT_NAME")
+var ghActor = os.Getenv("GITHUB_ACTOR")
+var ghWorkflow = os.Getenv("GITHUB_WORKFLOW")
+var ghActionsIDTokenRequestURL = os.Getenv("ACTIONS_ID_TOKEN_REQUEST_URL")
+var ghActionsIDTokenRequestToken = os.Getenv("ACTIONS_ID_TOKEN_REQUEST_TOKEN")
+var ghAPIURL = os.Getenv("GITHUB_API_URL")
+
+func listEnv() {
+	fmt.Println("Github Repo Owner: ", ghRepoOwner)
+	fmt.Println("Github Actor: ", ghActor)
+	fmt.Println("Github Ref: ", ghRef)
+	fmt.Println("Github Ref Name: ", ghRefName)
+	fmt.Println("Github API URL: ", ghAPIURL)
+	fmt.Println("Github Actions Token Request URL: ", ghActionsIDTokenRequestURL)
+	fmt.Println("Github Repo: ", ghRepo)
+	fmt.Println("Github Event: ", ghEvent)
+	fmt.Println("Github Workflow: ", ghWorkflow)
+	fmt.Println("Github Token: ", ghToken)
+	fmt.Println("Github Actions Request Token: ", ghActionsIDTokenRequestToken)
+	fmt.Printf("-----------------------\n")
+}
+
+func auth() {
+	if ghEvent != "pull_request" {
+		fmt.Println("Error: Not a pull request")
+	}
+	if ghRefName == "" {
+		fmt.Println("Error: Github Reference Name not available")
+	}
+
+	labels := []string{"test", "test2"}
+
+	requestBody, err := json.Marshal(map[string][]string{
+		"labels": labels,
+	})
+	fmt.Println(string(requestBody))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	prNumber := strings.Split(ghRefName, "/")[0]
+	url := ghAPIURL + "/repos/" + ghRepo + "/issues/" + prNumber + "/labels"
+	fmt.Println("URL: ", url)
+
+	timeout := time.Duration(5 * time.Second)
+	client := &http.Client{
+		Timeout: timeout,
+	}
+
+	request, err := http.NewRequest("POST", url, bytes.NewBuffer(requestBody))
+	// request, err := http.NewRequest("POST", url, nil)
+	request.Header.Add("Accept", "application/vnd.github.v3+json")
+	request.Header.Add("Authorization", "token "+ghToken)
+	request.Header.Set("Content-Type", "application/json")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	resp, err := client.Do(request)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf(string(body))
+}
 
 func main() {
-	ctx := context.Background()
-	token := os.Getenv("GTIHUB_TOKEN")
-
-	
-	if token == ""{
-		log.Fatal("Unauthorized: No token present")
-	} else {
-		fmt.Println("Success: Token aquired!")
-	}
+	listEnv()
+	fmt.Println()
+	auth()
 }
