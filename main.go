75a99acfafc4014bcc62ad69f3a9afe138ae7e6b
Vegard Arneberg <varneberg@gmail.com>
2022-05-05 11:25:42 +0200

    go-github auth test

--- main.go
+++ main.go
@@ -2,22 +2,92 @@ package main
 
 import (
 	// "context"
+	"errors"
 	"fmt"
+	"strings"
+
 	// "log"
 	"os"
 	// "syscall"
-
-	// "github.com/google/go-github/v44/github"
-	// "golang.org/x/oauth2"
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
 	// "golang.org/x/term"
 )
 
+const EnvBaseURL = "GITHUB_BASE_URL"
+type Config struct {
+	Token        string
+	BaseURL      string
+	Owner        string
+	Repo         string
+	CI           string
+}
+
+type Client struct {
+	*github.Client
+	Debug bool
+
+	Config Config
+
+	common service
+}
+
+type service struct {
+	client *Client
+}
+
+func NewClient() (*Client, error) {
+	var cfg Config
+	token := cfg.Token
+
+	if strings.HasPrefix(token, "$") {
+		token = os.Getenv(strings.TrimPrefix(token, "$"))
+	}
+
+	if token == "" {
+		fmt.Println("github token is missing")
+		return &Client{}, errors.New("github token is missing")
+	}
+
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(oauth2.NoContext, ts)
+	client := github.NewClient(tc)
+
+	baseURL := cfg.BaseURL
+	baseURL = strings.TrimPrefix(baseURL, "$")
+	if baseURL == EnvBaseURL {
+		baseURL = os.Getenv(EnvBaseURL)
+	}
+	if baseURL != "" {
+		var err error
+		client, err = github.NewEnterpriseClient(baseURL, baseURL, tc)
+		if err != nil {
+			fmt.Println("failed to create a new github api client")
+			return &Client{}, errors.New("failed to create a new github api client")
+		}
+	} 
+
+	c := &Client{
+		Config: cfg,
+		Client: client,
+	}
+
+
+	return c, nil
+
+}
+
 func main() {
+	NewClient()
+}
+// func main() {
 	//fmt.Print("GitHub Token: ")
 
-	for _, env := range os.Environ() {
-		fmt.Println(env)
-	}
+	// for _, env := range os.Environ() {
+	// 	fmt.Println(env)
+	// }
 	// byteToken, _ := term.ReadPassword(int(syscall.Stdin))
 	// println()
 	// token := string(byteToken)
@@ -45,4 +115,4 @@ func main() {
 	// }
 
 	// fmt.Printf("\n%v\n", github.Stringify(user))
-}
+// }
