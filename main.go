ea5f1277f5f26f25507158a978996a853841837e
Vegard Arneberg <varneberg@gmail.com>
2022-05-10 12:15:14 +0200

    label module init

--- main.go
+++ main.go
@@ -27,20 +27,6 @@ var (
 	ghAPIURL                     = os.Getenv("GITHUB_API_URL")
 )
 
-func listEnv() {
-	fmt.Println("Github Repo Owner: ", ghRepoOwner)
-	fmt.Println("Github Actor: ", ghActor)
-	fmt.Println("Github Ref: ", ghRef)
-	fmt.Println("Github Ref Name: ", ghRefName)
-	fmt.Println("Github API URL: ", ghAPIURL)
-	fmt.Println("Github Actions Token Request URL: ", ghActionsIDTokenRequestURL)
-	fmt.Println("Github Repo: ", ghRepo)
-	fmt.Println("Github Event: ", ghEvent)
-	fmt.Println("Github Workflow: ", ghWorkflow)
-	fmt.Println("Github Token: ", ghToken)
-	fmt.Println("Github Actions Request Token: ", ghActionsIDTokenRequestToken)
-	fmt.Printf("-----------------------\n")
-}
 
 func checkArgs() {
 	if len(os.Args[1:]) == 0 {
@@ -52,26 +38,56 @@ func checkArgs() {
 func checkEnv() {
 	if ghEvent != "pull_request" {
 		fmt.Println("Error: Not a pull request")
-		os.Exit(0)
+		os.Exit(2)
 	}
 	if ghRefName == "" {
 		fmt.Println("Error: Github Reference Name not available")
-		os.Exit(0)
+		os.Exit(2)
 	}
 }
 
-
-func postLabel(label Label) {
-	requestBody := parseLabel(label)
+// Returns URL to the active pull request
+func getPrUrl() string {
 	prNumber := strings.Split(ghRefName, "/")[0]
-	url := ghAPIURL + "/repos/" + ghRepo + "/issues/" + prNumber + "/labels"
-	fmt.Println("URL: ", url)
+	return ghAPIURL + "/repos/" + ghRepo + "/issues/" + prNumber + "/labels"
+}
 
+func getRepoUrl() string{
+	// https://api.github.com/repos/OWNER/REPO/labels
+	return ghAPIURL + "/repos/" + ghRepo + "/labels"
+}
+
+type flagSlice []string
+
+func(i *flagSlice) String() string {return ""}
+
+
+func(i *flagSlice) Set(value string) error {
+	*i = append(*i, strings.TrimSpace(value))
+	return nil
+}
+// colors
+// orange : #D93F0B
+type Label struct {
+	Name 					[]string 	  `json:"labels"`
+	Description 	  string 		`json:"description,omitempty"`
+	Color					  string  	`json:"color,omitempty"`
+}
+
+func parseLabel(label Label) []byte {
+	rb, err := json.Marshal(label)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(string(rb))
+	return rb
+}
+
+func apiRequest(requestBody []byte, url string, ){
 	timeout := time.Duration(5 * time.Second)
 	client := &http.Client{
 		Timeout: timeout,
 	}
-
 	request, err := http.NewRequest("POST", url, bytes.NewBuffer(requestBody))
 	request.Header.Add("Accept", "application/vnd.github.v3+json")
 	request.Header.Add("Authorization", "token "+ghToken)
@@ -89,61 +105,90 @@ func postLabel(label Label) {
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatalln(err)
+		os.Exit(2)
 	}
-	log.Printf(string(body))
+	fmt.Printf(string(body))
 	fmt.Println()
 }
 
-type arrayFlags []string
+// Adds labels to current pull request
+func addLabelPR(label Label) {
+	requestBody := parseLabel(label)
+	url := getPrUrl()
+	apiRequest(requestBody, url)
+	// requestBody := parseLabel(label)
+	// prNumber := strings.Split(ghRefName, "/")[0]
+	// url := ghAPIURL + "/repos/" + ghRepo + "/issues/" + prNumber + "/labels"
+	// url := getPrUrl()
+	// fmt.Println("PR_URL: ", url)
 
-func(i *arrayFlags) String() string {
-	return ""
+	// timeout := time.Duration(5 * time.Second)
+	// client := &http.Client{
+	// 	Timeout: timeout,
+	// }
+
+	// request, err := http.NewRequest("POST", url, bytes.NewBuffer(requestBody))
+	// request.Header.Add("Accept", "application/vnd.github.v3+json")
+	// request.Header.Add("Authorization", "token "+ghToken)
+	// request.Header.Set("Content-Type", "application/json")
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+
+	// resp, err := client.Do(request)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+	// defer resp.Body.Close()
+
+	// body, err := ioutil.ReadAll(resp.Body)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+
+	// fmt.Printf(string(body))
+	// fmt.Println()
+}
+
+func addLabelRepo(label Label){
 
 }
 
-func(i *arrayFlags) Set(value string) error {
-	*i = append(*i, strings.TrimSpace(value))
-	return nil
+func isFlagPassed(name string) bool {
+    found := false
+    flag.Visit(func(f *flag.Flag) {
+        if f.Name == name {
+            found = true
+        }
+    })
+    return found
 }
-// colors
-// orange : #D93F0B
-type Label struct {
-	Name 					[]string 	`json:"labels"`
-	Description 	string 	`json:"description,omitempty"`
-	Color					string  `json:"color,omitempty"`
-}
-
-func parseLabel(label Label) []byte {
-	rb, err := json.Marshal(label)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	fmt.Println(string(rb))
-	return rb
-}
-
 func main() {
 	checkArgs()
 	checkEnv()
-	// var label_name arrayFlags
+	var label_name flagSlice
 	labelcmd := flag.NewFlagSet("label", flag.ExitOnError)
-	label_name := labelcmd.String("n", "", "Name of the new label")
-	// labelcmd.Var(&label_name,"n", "Name of the label")
+	// label_name := labelcmd.String("n", "", "Name of the label")
+	labelcmd.Var(&label_name,"n", "Name of the label")
+	// label_newLabel := labelcmd.String("N", "", "Name new label to add")
 	label_desc := labelcmd.String("d", "", "Description of label, enclosed with \"\"")
 	label_color := labelcmd.String("c", "", "Color of label")
 	switch os.Args[1] {
 	case "label":
-		var labelList []string
+		// var labelList []string
 		labelcmd.Parse(os.Args[2:])
-		labelList = append(labelList, *label_name)
+		// labelList = append(labelList, *label_name)
+
+		fmt.Println(os.Args[2])
 		newLabel := Label{
-			Name: labelList,
+			Name: label_name,
 			Description: *label_desc,
 			Color: *label_color,
 		}
-		postLabel(newLabel)
+		addLabelPR(newLabel)
 		
 	default:
 		fmt.Println("Invalid arguments")
 	}
+	
 }
