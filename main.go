cf8f01770357a2d4682fc6e134befd257e7422d4
Hans Kristian Flaatten <hans@flaatten.org>
2022-05-23 15:09:08 +0200

    Add command flags (#17)
    * Add command flags
    
    * Add the required modules
    
    Co-authored-by: Vegard Arneberg <43135481+varneberg@users.noreply.github.com>

--- main.go
+++ main.go
@@ -4,6 +4,21 @@ import (
 	"fmt"
 	"github.com/varneberg/gaga/labels"
 	"os"
+	"strings"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+
+
+func checkEnv() {
+	if ghEvent != "pull_request" {
+		fmt.Println("Error: Not a pull request")
+		os.Exit(0)
+	}
+	if ghRefName == "" {
+		fmt.Println("Error: Github Reference Name not available")
 )
 
 // Check if any input was given
@@ -14,35 +29,76 @@ func checkArgs() {
 	}
 }
 
-func main() {
-	checkArgs()
-	switch os.Args[1] {
-	case "label":
-		labels.LabelHandler(os.Args[2:])
-	case "labeltest":
-		labels.TestLabelHandler(os.Args[2:])
+
+func parseLabel(label string) []byte {
+	var labels []string
+	labels = append(labels, label)
+	rb, err := json.Marshal(map[string][]string{
+		"labels": labels,
+	})
+	if err != nil {
+		log.Fatalln(err)
 	}
-	//labelcmd := flag.NewFlagSet("labels", flag.ExitOnError)
-	//// label_name := labelcmd.String("n", "", "Name of the labels")
-	//labelcmd.Var(&label_name, "n", "Name of the labels")
-	//// label_newLabel := labelcmd.String("N", "", "Name new labels to add")
-	//label_desc := labelcmd.String("d", "", "Description of labels, enclosed with \"\"")
-	//label_color := labelcmd.String("c", "", "Color of labels")
-	//switch os.Args[1] {
-	//case "labels":
-	//	// var labelList []string
-	//	labelcmd.Parse(os.Args[2:])
-	//	// labelList = append(labelList, *label_name)
-	//
-	//	fmt.Println(os.Args[2])
-	//	newLabel := newLabel{
-	//		Name:        label_name,
-	//		Description: *label_desc,
-	//		Color:       *label_color,
-	//	}
-	//	addLabelPR(newLabel)
-	//
-	//default:
-	//	fmt.Println("Invalid arguments")
-	//}
+	return rb
 }
+
+func postLabel(label string) {
+	requestBody := parseLabel(label)
+	prNumber := strings.Split(ghRefName, "/")[0]
+	url := ghAPIURL + "/repos/" + ghRepo + "/issues/" + prNumber + "/labels"
+	fmt.Println("URL: ", url)
+
+	timeout := time.Duration(5 * time.Second)
+	client := &http.Client{
+		Timeout: timeout,
+	}
+
+	request, err := http.NewRequest("POST", url, bytes.NewBuffer(requestBody))
+	request.Header.Add("Accept", "application/vnd.github.v3+json")
+	request.Header.Add("Authorization", "token "+ghToken)
+	request.Header.Set("Content-Type", "application/json")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	resp, err := client.Do(request)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Printf(string(body))
+	fmt.Println()
+}
+
+func main() {
+	checkEnv()
+	fmt.Println()
+
+	var labelName string
+	var labelColor string
+	var labelDescription string
+
+	var cmdLabel = &cobra.Command{
+		Use:   "label [label]",
+		Short: "Label a pull request",
+		Long:  `label is for labeling a pull request.`,
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			postLabel(strings.Join(args, " "))
+		},
+	}
+
+	cmdLabel.Flags().StringVarP(&labelName, "name", "n", "", "label name")
+	cmdLabel.Flags().StringVarP(&labelColor, "color", "c", "", "label color")
+	cmdLabel.Flags().StringVarP(&labelDescription, "description", "d", "", "label description")
+
+	var rootCmd = &cobra.Command{Use: "app"}
+	rootCmd.AddCommand(cmdLabel)
+	rootCmd.Execute()
+}
+
