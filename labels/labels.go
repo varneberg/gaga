bfb01fde6617813957f5e232013d5101932f64a5
Vegard Arneberg <varneberg@gmail.com>
2022-06-27 13:52:34 +0200

    bug fixes

--- labels/labels.go
+++ labels/labels.go
@@ -69,7 +69,8 @@ func AddLabelPR(labelName string) {
 	body := parseLabelName(labelName)
 	//fmt.Println("Api Request Body: ", string(body))
 	status, resp := requests.SendRequest("POST", url, body)
-	requests.PrintResponse(status, resp)
+	// requests.PrintResponse(status, resp)
+	requests.CheckRespError(status, resp)
 }
 
 func toList(inputString string) []string {
@@ -83,14 +84,16 @@ func removeLabel(labelName string) {
 	//var body []byte
 	body := parseLabelName(labelName)
 	status, body := requests.SendRequest("DELETE", url, body)
-	requests.PrintResponse(status, body)
+	// requests.PrintResponse(status, body)
+	requests.CheckRespError(status, body)
 }
 
 // Remove all labels from a pull request
 func removeAllLabels() {
 	url := requests.GetPRUrl()
 	status, body := requests.SendRequest("DELETE", url, nil)
-	requests.PrintResponse(status, body)
+	// requests.PrintResponse(status, body)
+	requests.CheckRespError(status, body)
 }
 
 func createNewLabel(label newLabel) {
@@ -108,7 +111,8 @@ func createNewLabel(label newLabel) {
 		log.Fatalln(err)
 	}
 	status, respbody := requests.SendRequest("POST", url, body)
-	requests.PrintResponse(status, respbody)
+	// requests.PrintResponse(status, respbody)
+	requests.CheckRespError(status, respbody)
 	AddLabelPR(label.Name)
 }
 
